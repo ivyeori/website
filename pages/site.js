799f58c9ded79d56ffc8cc6181b99120d9ad9328
Alexander Graf <mail@getalex.info>
2023-01-05 19:43:10 +0000

    fixed the text

--- pages/site.js
+++ pages/site.js
@@ -37,9 +37,17 @@ function main() {
                 <p className="my-6 text-3xl text-center text-white ">
                     Hi, I&#x27;m Alex
                 </p>
-                <h2 className="max-w-3xl py-2 mx-auto text-5xl font-bold text-center text-white md:text-6xl dark:text-white">
-                    Building websites, programs, and experiences.
-                </h2>
+                <div className="max-w-3xl py-2 mx-auto text-5xl font-bold text-center text-white md:text-6xl dark:text-white">
+                   <h2>
+                   Building websites,
+                   </h2>
+                   <h2>
+                    programs 
+                   </h2>
+                   <h2>
+                   and experiences.
+                   </h2>
+                </div>
                 <div className="flex items-center justify-center mt-4">
                     <a href="https://beacons.ai/ivyeori" target="_blank" className="px-4 py-2 my-2 text-white uppercase bg-transparent border-2 border-gray-800 md:mt-16 dark:text-white dark:bg-white hover:dark:bg-gray-100 dark:text-white hover:bg-gray-800 hover:text-white text-md">
                         CONNECT WITH ME
